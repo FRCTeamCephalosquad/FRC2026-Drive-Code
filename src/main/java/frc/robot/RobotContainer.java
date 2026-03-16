3cf7594f5bd6daa7cee0e052f97a7bc6d8eb6010
bkuker <bkuker@billkuker.com>
2026-03-15 20:19:25 -0400

    two autos

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,6 +4,7 @@
 
 package frc.robot;
 
+import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -15,13 +16,12 @@ import static frc.robot.Constants.OperatorConstants.*;
 import java.util.function.Supplier;
 
 import frc.robot.autos.ScootAndShoot;
+import frc.robot.autos.ScootShootAndClimb;
 import frc.robot.commands.AutoAimCommand;
 import frc.robot.commands.ClimbPosition;
 import frc.robot.commands.balls.Eject;
 import frc.robot.commands.balls.Intake;
 import frc.robot.commands.balls.LaunchSequence;
-import frc.robot.commands.climb.ClimbDown;
-import frc.robot.commands.climb.ClimbUp;
 import frc.robot.commands.drive.JoystickTankDriveCommand;
 import frc.robot.commands.drive.OrientInDirectionCommand;
 import frc.robot.subsystems.DriveSubsystem;
@@ -37,15 +37,17 @@ public class RobotContainer {
     private final DriveSubsystem driveSubsystem = new DriveSubsystem();
     private final FuelSubsystem fuelSubsystem = new FuelSubsystem();
     private final ClimberSubsystem climberSubsystem = new ClimberSubsystem();
-    private final PoseSubsystem poseSubsystem = new PoseSubsystem(driveSubsystem);
     private final HopperSubsystem hopperSubsystem = new HopperSubsystem();
+
+    private final PoseSubsystem poseSubsystem = new PoseSubsystem(driveSubsystem);
+    private final Supplier<Pose2d> getPose = poseSubsystem::getCurrentPose;
+
+    @SuppressWarnings("unused") // Gets registered so periodic is called
     private final PDUSubsystem pduSubsystem = new PDUSubsystem();
 
     // CONTROLLERS
-    private final CommandXboxController driverController = new CommandXboxController(
-            DRIVER_CONTROLLER_PORT);
-    private final CommandXboxController operatorController = new CommandXboxController(
-            OPERATOR_CONTROLLER_PORT);
+    private final CommandXboxController driverController = new CommandXboxController(DRIVER_CONTROLLER_PORT);
+    private final CommandXboxController operatorController = new CommandXboxController(OPERATOR_CONTROLLER_PORT);
 
     // The autonomous chooser
     private final SendableChooser<Supplier<Command>> autoChooser = new SendableChooser<>();
@@ -56,7 +58,11 @@ public class RobotContainer {
     public RobotContainer() {
         configureBindings();
         autoChooser.setDefaultOption("Shoot & Scoot",
-                () -> new ScootAndShoot(driveSubsystem, fuelSubsystem, climberSubsystem, poseSubsystem::getCurrentPose));
+                () -> new ScootAndShoot(driveSubsystem, fuelSubsystem, getPose));
+
+        autoChooser.addOption("Scoot Shoot & Climb",
+                () -> new ScootShootAndClimb(driveSubsystem, fuelSubsystem, climberSubsystem, getPose));
+
         SmartDashboard.putData("Auto choices", autoChooser);
     }
 
@@ -64,69 +70,58 @@ public class RobotContainer {
      * Use this method to define your trigger->command mappings.
      */
     private void configureBindings() {
-        /*
-         * driverController.a().whileTrue(driveSubsystem.sysIdQuasistaticForward());
-         * driverController.b().whileTrue(driveSubsystem.sysIdQuasistaticBackward());
-         * driverController.x().whileTrue(driveSubsystem.sysIdDynamicForward());
-         * driverController.y().whileTrue(driveSubsystem.sysIdDynamicBackward());
-         */
 
-        // Drive Default
-        /*
-         * *
-         * driveSubsystem.setDefaultCommand(
-         * new JoystickArcadeDriveCommand(
-         * () -> -driverController.getLeftY(),
-         * () -> -driverController.getLeftX(),
-         * driveSubsystem));
-         */
+        // Fuesl and Climber both default to stop
+        fuelSubsystem.setDefaultCommand(fuelSubsystem.run(() -> fuelSubsystem.stop()));
+        climberSubsystem.setDefaultCommand(climberSubsystem.run(() -> climberSubsystem.stop()));
+
+        // OPERATOR CONTROLS
+
+        // Auto Aim
+        operatorController.a().whileTrue(new AutoAimCommand(driveSubsystem,
+                getPose));
+
+        // Ball Handling
+        operatorController.leftTrigger().whileTrue(new Intake(fuelSubsystem));
+        operatorController.rightTrigger().whileTrue(new LaunchSequence(fuelSubsystem, hopperSubsystem));
+        operatorController.start().whileTrue(new Eject(fuelSubsystem));
+
+        // Hopper Control
+        operatorController.povDown().whileTrue(
+                Commands.sequence(
+                        climberSubsystem.Reset(),
+                        hopperSubsystem.lowerCommand()));
+        operatorController.povUp().whileTrue(hopperSubsystem.raiseCommand());
+        operatorController.y().whileTrue(hopperSubsystem.buttWiggle());
+
+        // DRIVER CONTROLS
+
+        // General Tank Drive
         driveSubsystem.setDefaultCommand(
                 new JoystickTankDriveCommand(
                         () -> -driverController.getLeftY(),
                         () -> -driverController.getRightY(),
                         driveSubsystem));
 
-        // Fuesl and Climber both default to stop
-        fuelSubsystem.setDefaultCommand(fuelSubsystem.run(() -> fuelSubsystem.stop()));
-        climberSubsystem.setDefaultCommand(climberSubsystem.run(() -> climberSubsystem.stop()));
+        // Field relative D-PAD helpers
+        driverController.povUp()
+                .whileTrue(new OrientInDirectionCommand(driveSubsystem, getPose, 0));
+        driverController.povLeft()
+                .whileTrue(new OrientInDirectionCommand(driveSubsystem, getPose, 90));
+        driverController.povDown()
+                .whileTrue(new OrientInDirectionCommand(driveSubsystem, getPose, 180));
+        driverController.povRight()
+                .whileTrue(new OrientInDirectionCommand(driveSubsystem, getPose, 270));
 
-        // Test orientation code
-        operatorController.a().whileTrue(new AutoAimCommand(driveSubsystem,
-                poseSubsystem::getCurrentPose));
-
-        // Some probably stupid operator commands
-        operatorController.leftTrigger().whileTrue(new Intake(fuelSubsystem));
-        operatorController.rightTrigger().whileTrue(new LaunchSequence(fuelSubsystem, hopperSubsystem));
-        operatorController.start().whileTrue(new Eject(fuelSubsystem));
-
-        driverController.a().and(driverController.start().negate()).whileTrue(
-                Commands.sequence(hopperSubsystem.raiseCommand(),
-                climberSubsystem.MoveToReady())
-                );
-        driverController.y().and(driverController.start().negate()).whileTrue(climberSubsystem.Climb());
+        // Climber Related
+        driverController.a().whileTrue(
+                Commands.sequence(
+                        hopperSubsystem.raiseCommand(),
+                        climberSubsystem.MoveToReady()));
+        driverController.y().whileTrue(climberSubsystem.Climb());
         driverController.b().whileTrue(climberSubsystem.Reset());
 
-        driverController.start().and(driverController.a()).whileTrue(new ClimbDown(climberSubsystem));
-        driverController.start().and(driverController.y()).whileTrue(new ClimbUp(climberSubsystem));
-
-        driverController.x().whileTrue(new ClimbPosition(driveSubsystem, poseSubsystem::getCurrentPose));
-
-        driverController.povUp()
-                .whileTrue(new OrientInDirectionCommand(driveSubsystem, poseSubsystem::getCurrentPose, 0));
-        driverController.povLeft()
-                .whileTrue(new OrientInDirectionCommand(driveSubsystem, poseSubsystem::getCurrentPose, 90));
-        driverController.povDown()
-                .whileTrue(new OrientInDirectionCommand(driveSubsystem, poseSubsystem::getCurrentPose, 180));
-        driverController.povRight()
-                .whileTrue(new OrientInDirectionCommand(driveSubsystem, poseSubsystem::getCurrentPose, 270));
-
-        operatorController.povDown().whileTrue(
-                Commands.repeatingSequence(
-                        climberSubsystem.Reset(),
-                        hopperSubsystem.lowerCommand()
-        ));
-        operatorController.povUp().whileTrue(hopperSubsystem.raiseCommand());
-        operatorController.y().whileTrue(hopperSubsystem.buttWiggle());
+        driverController.x().whileTrue(new ClimbPosition(driveSubsystem, getPose));
 
     }
 
